2097a0dfc01d9bda4f3325cf14e893762fd502ba
ayu-tw <80678206+ayu-tw@users.noreply.github.com>
2021-04-29 20:09:04 +0200

    added functions for updating current location, weather description, temperature, humidity and wind speed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-function getCurrentCity(response) {
-  console.log(response);
+function updateCurrentInfo(response) {
+  let cityName = document.querySelector("#selected-city");
+  cityName.innerHTML = `${response.data.name}`;
+  let description = document.querySelector("#local-weather-description");
+  description.innerHTML = `${response.data.weather[0].description}`;
+  let temp = document.querySelector("#current-temp");
+  temp.innerHTML = `${response.data.main.temp}`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+  wind.innerHTML = `${windSpeed} km/h`;
+  let visibility = document.querySelector("#visibility");
+  visibility.innerHTML = `${response.data.visibility}`;
 }
-let apiKey = "05348ae2e09beca97cb2165f14ee5d2b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(getCurrentCity);
+
+function defaultPage(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  city = city.trim();
+  let apiKey = "05348ae2e09beca97cb2165f14ee5d2b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCurrentInfo);
+}
+
+let search = document.querySelector("#submit-button");
+search.addEventListener("click", defaultPage);
